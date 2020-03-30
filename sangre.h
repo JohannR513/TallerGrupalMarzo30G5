836a20b28a7f5766b3116f2afc052a3d647da0dd
byzokky <57722802+byzokky@users.noreply.github.com>
2020-03-30 17:04:34 -0500

    Create sangre.h

---
+++ sangre.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct paciente{
+	char nombre[ 15 ];
+	int edad, cedula, tipoSangre;
+}paciente;
+
+typedef struct donaciones{
+	char nombre[ 15 ];
+	int edad, cedula, tipoSangre;
+}donaciones;
+
+typedef enum tipoSangre{
+   	APOS = 1,
+   	BPOS = 2,
+   	OPOS = 3,
+   	ABPOS = 4,
+   	ANEG = 5,
+   	BNEG = 6,
+   	ONEG = 7,
+   	ABNEG = 8
+}tipoSangre;
