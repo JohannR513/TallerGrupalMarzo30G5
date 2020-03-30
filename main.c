0663c2300ed0a12044d7014caf484353b959df02
byzokky <57722802+byzokky@users.noreply.github.com>
2020-03-30 17:31:54 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,4 +4,23 @@
 
 int main(){
 
+do{
+ opc = menu( );
+switch ( opc ){
+   case 1: ingresarDonador(){
+       break;
+   case 2: 
+       break;
+   case 3:
+      break;
+   case 4:
+      break;
+   case 0:
+      break;
+
+  default: printf( "Ingresa una opcion valida" );
+  }while( opcion != 0 );
+
+}
+}
 }
