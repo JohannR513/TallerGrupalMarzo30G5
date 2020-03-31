27fd3ddf3124275f800e97d2fdd8a32e2aa05025
byzokky <57722802+byzokky@users.noreply.github.com>
2020-03-31 10:32:03 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,24 +2,31 @@
 
 
 int main(){
+User mat[8][MAX_CAP];
+int counts[8] = {};
+int groupSizes[8] = {};
+
+int cap;
+printf("Capacidad: ");
+scanf("%d", &cap);
 
 do{
  opc = menu( );
 switch ( opc ){
-   case 1: ingresarDonador( ){
+   case 1: agrgarP( );
        break;
-   case 2: 
+   case 2: eliminarP( ); 
        break;
-   case 3:
+   case 3: agregarD( );
       break;
-   case 4:
+   case 4: eliminarD( );
+      break;
+   case 5: transfusion( );
       break;
    case 0:
       break;
-
   default: printf( "Ingresa una opcion valida" );
   }while( opcion != 0 );
 
-}
 }
 }
