231848980f8bc2575cef31ff26f5564fb543e96b
byzokky <57722802+byzokky@users.noreply.github.com>
2020-03-31 11:47:12 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,66 +1,150 @@
 #include "sangre.h"
 
-typedef struct paciente{
-	char nombre[ 15 ];
-	int edad, cedula, tipoSangre;
-}paciente;
-
-typedef struct donaciones{
-	char nombre[ 15 ];
-	int edad, cedula, tipoSangre;
-}donaciones;
-
-typedef enum tipoSangre{
-   	APOS = 1,
-   	BPOS = 2,
-   	OPOS = 3,
-   	ABPOS = 4,
-   	ANEG = 5,
-   	BNEG = 6,
-   	ONEG = 7,
-   	ABNEG = 8
-}tipoSangre;
-   
-int menu( ){
-	
+int menu()
+{
 	int opc;
     printf( "MENU:\n");
     printf( "=====================\n" );
     printf( "1: Ingresar donador\n" );
-    printf( "2: ver muestas\n" );
-    printf( "3: transfusio\n" );
-    printf( "4: agregar paciente\n" );
+    printf( "2: ver muestras\n" );
+    printf( "3: buscar sangre\n" );
+    printf( "4: donar\n" );
     printf( "0: Salir\n" );
     scanf( "%d", &opc );
 
     return opc;
-
-do{
-   opc = menu( );
-	switch ( opc ){
-		default: printf( "Ingresa una opcion valida" );
-			
-		 
-	}while( opcion != 0 );
-
 }
 
+void donador(donante* punt)
+{
+    int sangre;
+    printf("Nombre: ");
+    scanf("%s", punt->name);
+    printf("edad: ");
+    scanf("%s", punt->edad);
+
+    printf("Tipo de sangre: \n");
+    printf("1: A+\n");
+    printf("2: B+\n");
+    printf("3: O+\n");
+    printf("4: AB+\n");
+    printf("5: A-\n");
+    printf("6: B-\n");
+    printf("7: O-\n");
+    printf("8: AB-\n");
+    scanf("%d", &sangre);
+    punt->sangre = sangre;
+
+    punt->donacion = 0;
 }
 
+void IngresarD(donante matriz[][CAP], int* RH, int cap)
+{
+    donante punt;
+    donador(&punt);
 
+    if (RH[punt.sangre] < cap)
+    {
+        matriz[punt.sangre][RH[punt.sangre]] = punt;
+        RH[punt.sangre]++;
+    }
+    else
+    {
+        printf("no se puede donar\n");
+    }
+}
 
+void muestras(int *tipo)
+{
+    int i;
+    for (i = 0; i < 8; i++)
+    {
+        switch (i)
+        {
+            case 0:
+                printf("A+: ");
+                break;
+            case 1:
+                printf("B+: ");
+                break;
+            case 2:
+                printf("O+: ");
+                break;
+            case 3:
+                printf("AB+: ");
+                break;
+            case 4:
+                printf("A-: ");
+                break;
+            case 5:
+                printf("B-: ");
+                break;
+            case 6:
+                printf("O-: ");
+                break;
+            case 7:
+                printf("AB-: ");
+                break;
+        }
+        printf("%d\n", tipo[i]);
+    }
+}
 
+void buscarSangre(donante matriz[][CAP], int *RH)
+{
+    int i;
+    int sangre;
+    printf("Tipo de sangre: \n");
+    printf( "=====================\n" );
+    printf("1: A+\n");
+    printf("2: B+\n");
+    printf("3: O+\n");
+    printf("4: AB+\n");
+    printf("5: A-\n");
+    printf("6: B-\n");
+    printf("7: O-\n");
+    printf("8: AB-\n");
+    scanf("%d", &sangre);
 
+    printf("Donante:\n");
+    for (i = 0; i < RH[sangre]; i++)
+    {
+        printf("Nombre: %s\n", matriz[sangre][i].name);
+        printf("edad: %s\n", matriz[sangre][i].edad);
+        printf("donacion: %d\n", matriz[sangre][i].donacion);
+        printf("===============\n");
+    }
+}
 
+void donar(donante matriz[][CAP], int *tipo, int cap)
+{
+    char name[10];
+    int donacion;
+    int i, j, num = 0;
+    printf("Nombre: ");
+    scanf("%s", name);
+    printf("donacion: ");
+    scanf("%d", &donacion);
 
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < cap; j++)
+        {
+            if (!strcmp(name, matriz[i][j].name))
+            {
+                matriz[i][j].donacion += donacion;
+                num = 1;
+                break;
+            }
+        }
+        
+        if (num)
+        {
+            tipo[i] += donacion;
+            break;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
+    if (!num)
+        printf("Este usuario no existe\n");
+}
